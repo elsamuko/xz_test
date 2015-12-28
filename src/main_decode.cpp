e865bd9cdc205582b5c5f6359e236184cf002bc1
Samuel Albrecht <elsamuko@gmail.com>
2015-12-28 22:58:08 +0000

    fix: win: explicitly set stdin/stdout to binary

--- src/main_decode.cpp
+++ src/main_decode.cpp
@@ -1,6 +1,22 @@
 #include <xz/decode.hpp>
 
+#if _WIN32
+#include <assert.h>
+#include <fcntl.h>
+#include <io.h>
+#endif // _WIN32
+
 int main() {
+
+#if _WIN32
+    // set I/O to binary to avoid EOL casts
+    int result = 0;
+    result = _setmode( _fileno( stdin ), _O_BINARY );
+    assert( result != -1 );
+    result = _setmode( _fileno( stdout ), _O_BINARY );
+    assert( result != -1 );
+#endif // _WIN32
+
     bool success = xz::deflate( std::cin, std::cout );
     fclose( stdout );
     return success ? EXIT_SUCCESS : EXIT_FAILURE;
