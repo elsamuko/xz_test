98aef4039d8342042b37792021efcd1dcc6ba13e
Samuel Albrecht <elsamuko@gmail.com>
2015-12-28 21:09:07 +0100

    chg: updated unit test

--- testing/roundtrip_test/roundtrip_test.cpp
+++ testing/roundtrip_test/roundtrip_test.cpp
@@ -34,7 +34,7 @@ void Roundtrip_test::testString() {
                              "0123456789"
                              "0123456789";
 
-    for( int i = 0; i < 100; ++i ) {
+    for( size_t i = 0; i < 100; ++i ) {
         std::string original = longString.substr( 0, i );
         std::string compressed;
         std::string deflated;
@@ -56,7 +56,7 @@ void Roundtrip_test::testString() {
         }
 
         QVERIFY2( !compressed.empty(), std::to_string( i ).c_str() );
-        QVERIFY2( !deflated.empty(), std::to_string( i ).c_str() );
+        QVERIFY2( deflated.size() == i, std::to_string( i ).c_str() );
         QVERIFY2( deflated == original, std::to_string( i ).c_str() );
     }
 }
@@ -75,35 +75,48 @@ namespace utils {
 
 void Roundtrip_test::testFile() {
 
-    std::string data = "d8LUeENFcqcwTdRH2cgifU1K9tp7pMmHErQeQLh5U0k58ybMHR";
-    std::string compressed;
-    std::string deflated;
+    std::string longString = "0123456789"
+                             "0123456789"
+                             "0123456789"
+                             "0123456789"
+                             "0123456789"
+                             "0123456789"
+                             "0123456789"
+                             "0123456789"
+                             "0123456789"
+                             "0123456789";
 
-    std::string filename_original   = "original.txt";
-    std::string filename_compressed = "compressed.xz";
-    std::string filename_deflated   = "deflated.txt";
+    for( size_t i = 0; i < 100; ++i ) {
+        std::string data = longString.substr( 0, i );
+        std::string compressed;
+        std::string deflated;
 
-    utils::toFile( filename_original.c_str(), data );
+        std::string filename_original   = "original.txt";
+        std::string filename_compressed = "compressed.xz";
+        std::string filename_deflated   = "deflated.txt";
 
-    // compress
-    {
-        std::ifstream in( filename_original.c_str(), std::ifstream::in | std::ifstream::binary );
-        std::ofstream out( filename_compressed.c_str(), std::ofstream::out | std::ofstream::binary );
-        xz::compress( in, out );
+        utils::toFile( filename_original.c_str(), data );
+
+        // compress
+        {
+            std::ifstream in( filename_original.c_str(), std::ifstream::in | std::ifstream::binary );
+            std::ofstream out( filename_compressed.c_str(), std::ofstream::out | std::ofstream::binary );
+            xz::compress( in, out );
+        }
+        compressed = utils::fromFile( filename_compressed );
+
+        // deflate
+        {
+            std::ifstream in( filename_compressed.c_str(), std::ifstream::in | std::ifstream::binary );
+            std::ofstream out( filename_deflated.c_str(), std::ofstream::out | std::ofstream::binary );
+            xz::deflate( in, out );
+        }
+        deflated = utils::fromFile( filename_deflated );
+
+        QVERIFY( !compressed.empty() );
+        QVERIFY( deflated.size() == i );
+        QVERIFY( deflated == data );
     }
-    compressed = utils::fromFile( filename_compressed );
-
-    // deflate
-    {
-        std::ifstream in( filename_compressed.c_str(), std::ifstream::in | std::ifstream::binary );
-        std::ofstream out( filename_deflated.c_str(), std::ofstream::out | std::ofstream::binary );
-        xz::deflate( in, out );
-    }
-    deflated = utils::fromFile( filename_deflated );
-
-    QVERIFY( !compressed.empty() );
-    QVERIFY( !deflated.empty() );
-    QVERIFY( deflated == data );
 }
 
 QTEST_MAIN( Roundtrip_test )
